b9f34852a6735c780e70b7e5fc9f047894770723
SamuelLJG <samuelljg@outlook.com>
2025-04-23 01:55:56 -0300

    test for deploy

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <body>
+            
+        <div>
+            <p>
+                inicio
+            </p>
+        </div>
+        </body>
+    )
+}
