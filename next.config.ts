bd68c47a7c9766dd685cf7b6758174d172512baf
SamuelLJG <samuelljg@outlook.com>
2025-04-26 18:26:31 -0300

    image test fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 // next.config.ts
 const nextConfig: NextConfig = {
   images: {
-    domains: ['gi.yatta.moe','enka.network'], // ✅ adiciona o domínio aqui
+    domains: ['gi.yatta.moe','enka.network'],
+    unoptimized: true, // ✅ adiciona o domínio aqui
   },
 };
 
