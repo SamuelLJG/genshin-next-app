9a393ce02a2937b6c2c8ee3ac2b0f43d07607602
SamuelLJG <samuelljg@outlook.com>
2025-04-26 18:31:18 -0300

    image test fix

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 // next.config.ts
 const nextConfig: NextConfig = {
   images: {
-    domains: ['gi.yatta.moe','enka.network'],
-    unoptimized: true,
+    domains: ['gi.yatta.moe','enka.network']
   },
 };
 
