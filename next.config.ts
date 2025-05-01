6cbb20b8ffe43b78de139980f46df8946af6f032
SamuelLJG <samuelljg@outlook.com>
2025-05-01 12:53:37 -0300

    sitemap test

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['gi.yatta.moe','enka.network', 'api.hakush.in']
   },
+  async redirects() {
+    return [
+      {
+        source: '/times',
+        destination: '/teams',
+        permanent: false,
+      }
+    ];
+  }
 };
 
 export default nextConfig;
