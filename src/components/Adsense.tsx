11b2aafc351131683f8efb673eb4b1a5ecb63d96
SamuelLJG <samuelljg@outlook.com>
2025-05-07 03:06:15 -0300

    all adsense ads added

--- src/components/Adsense.tsx
+++ src/components/Adsense.tsx
@@ -18,14 +18,16 @@ const AdComponent: React.FC<AdComponentProps> = ({adFormat = 'auto', adLayout = 
   }, []);
 
   return (
+    <div className='adbox'>
     <ins className="adsbygoogle"
-         style={{display:'block', margin: '0px auto', width:'100%'}}
+         style={{display:'block', margin: '0px auto'}}
          data-ad-client="ca-pub-1999593447203691"
          data-ad-slot='5225041946'
          data-ad-format={adFormat}
          data-ad-layout={adLayout}
          data-full-width-responsive="true"
          ></ins>
+    </div>
   );
 };
 
