c6f6c99873e97bc8bc414bcc47dc91ee094cea42
SamuelLJG <samuelljg@outlook.com>
2025-05-07 01:10:45 -0300

    728test2

--- src/components/Adsense.tsx
+++ src/components/Adsense.tsx
@@ -2,12 +2,9 @@
 
 import React, { useEffect } from 'react';
 
-interface AdComponentProps {
-  adFormat?: string;
-  adLayout?: string;
-}
 
-const AdComponent: React.FC<AdComponentProps> = ({adFormat = 'auto', adLayout = '' }) => {
+
+const AdComponent: React.FC = () => {
   useEffect(() => {
     try {
       (window as any).adsbygoogle = (window as any).adsbygoogle || [];
@@ -19,11 +16,9 @@ const AdComponent: React.FC<AdComponentProps> = ({adFormat = 'auto', adLayout = 
 
   return (
     <ins className="adsbygoogle"
-         style={{display:'inline-block',width:'728px',height:'90px', maxHeight: '90px'}}
+         style={{display:'inline-block',width:'728px',height:'90px'}}
          data-ad-client="ca-pub-1999593447203691"
          data-ad-slot='4805769734'
-         data-ad-format={adFormat}
-         data-ad-layout={adLayout}
          ></ins>
   );
 };
