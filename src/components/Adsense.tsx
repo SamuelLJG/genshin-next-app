2ca8864025be3e96d066d0eaa71391a8b4397f34
SamuelLJG <samuelljg@outlook.com>
2025-05-07 00:38:53 -0300

    ad privacypolicy test

--- src/components/Adsense.tsx
+++ src/components/Adsense.tsx
@@ -4,9 +4,10 @@ import React, { useEffect } from 'react';
 
 interface AdComponentProps {
   adFormat?: string;
+  adLayout?: string;
 }
 
-const AdComponent: React.FC<AdComponentProps> = ({adFormat = 'horizontal' }) => {
+const AdComponent: React.FC<AdComponentProps> = ({adFormat = 'horizontal' , adLayout = ''}) => {
   useEffect(() => {
     try {
       (window as any).adsbygoogle = (window as any).adsbygoogle || [];
@@ -23,6 +24,7 @@ const AdComponent: React.FC<AdComponentProps> = ({adFormat = 'horizontal' }) => 
          data-ad-client="ca-pub-1999593447203691"
          data-ad-slot= '8942974229'
          data-ad-format={adFormat}
+         data-ad-layout={adLayout}
          data-full-width-responsive="true"></ins>
     </div>
   );
