1a0c6bd92445d57a8197550cc2f75905f0d624b8
SamuelLJG <samuelljg@outlook.com>
2025-05-08 17:43:15 -0300

    test novo2

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
- import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 
 // next.config.ts
@@ -36,9 +35,5 @@ const nextConfig: NextConfig = {
     ];
   }
 };
-
- if (process.env.NODE_ENV === 'development') {
-   await setupDevPlatform();
- }
 
 export default nextConfig;
