920b5734624969b79e673aea9852b715720e28a9
SamuelLJG <samuelljg@outlook.com>
2025-05-08 16:19:49 -0300

    cloudflare test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Filter from "@/components/full-filter";
 import ptBr from "@/data/pt-br.json"
 import AdComponent from "@/components/Adsense";
 
+export const runtime = "edge";
+
 export default function Home() {
     function formatarNome(nome:string) {
         return nome
