47f943015b595c1553ecfe449293984b3c7d5e9f
SamuelLJG <samuelljg@outlook.com>
2025-05-09 16:01:02 -0300

    ezoic removed

--- src/components/Adsense.tsx
+++ src/components/Adsense.tsx
@@ -1,9 +1,30 @@
+'use client';
 
+// components/AdsenseAd.js
+import { useEffect } from "react";
 
 export default function AdsenseAd() {
 
+  useEffect(() => {
+    try {
+      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push(
+        {}
+      );
+    } catch (e) {
+      console.error('Error loading ads:', e);
+    }
+  }, []);
 
   return (
-    ''
-  )
+    <div className="adbox">
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-1999593447203691"
+        data-ad-slot="5225041946"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      </div>
+  );
 }
