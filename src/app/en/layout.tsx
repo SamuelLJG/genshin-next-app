401b03e61ebd7fa2310abb88126d7445beec93a7
SamuelLJG <samuelljg@outlook.com>
2025-05-10 08:35:21 -0300

    teste

---
+++ src/app/en/layout.tsx
@@ -0,0 +1,55 @@
+import { Anuphan, Plus_Jakarta_Sans } from 'next/font/google'
+import { Metadata } from "next";
+import Nav from '@/components/nav';
+import Footer from '@/components/footer';
+import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script';
+import { state } from "@/components/config";
+
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://genshinbuild.com'),
+  icons: {
+    icon: '/favicon-96x96.png', // caminho dentro de /public
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      'max-image-preview': 'large'
+    }
+  }
+}
+
+
+const anuphan = Anuphan({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export default async function RootLayout({ children }: {children: React.ReactNode;}) {
+
+  return (
+    <html lang="en">
+      <GoogleAnalytics gaId="G-ZMW5Q2STCE" />
+      <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1999593447203691"
+            crossOrigin="anonymous"
+          ></Script>
+      <body>
+        <Nav/>
+        {children}
+        <Footer/>
+      </body>
+    </html>
+  );
+}
