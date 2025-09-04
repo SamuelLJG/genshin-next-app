8bb2154aa8c8185ef6f0a647e9f42905e322e801
SamuelLJG <samuelljg@outlook.com>
2025-09-03 22:45:54 -0300

    adsterra test

--- src/components/Adsense2.tsx
+++ src/components/Adsense2.tsx
@@ -12,17 +12,7 @@ const AdBanner = () => {
   }, []);
 
   return (
-    <div className='adbox ptdesc'>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1999593447203691"
-     crossOrigin="anonymous"></script>
-    <ins className="adsbygoogle"
-               style={{display:'block'}}
-               data-ad-client="ca-pub-1999593447203691"
-               data-ad-slot="8181198147"
-               data-ad-format="auto"
-               data-full-width-responsive="true"
-    />
-    </div>
+    ''
   );
 };
 export default AdBanner;
