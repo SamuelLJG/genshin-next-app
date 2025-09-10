d2bc9ceb31a8965139a938cb4f81fa0238a161cb
SamuelLJG <samuelljg@outlook.com>
2025-09-10 20:11:29 -0300

    teste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,13 +38,7 @@ export default async function RootLayout({ children }: {children: React.ReactNod
 
   return (
         <>
-            <Script
-  async
-  strategy="afterInteractive"
-  src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1999593447203691`}
-  crossOrigin="anonymous"
-/>
-        <GoogleAnalytics gaId="G-ZMW5Q2STCE" />
+            
         {children}
         </>
   )
